a54fb0649ae3fef893042316ebc349d103284288
Minh Đỗ <minhdo@CauBeNguNger.local>
2026-04-16 12:22:45 +0700

    add topic/class-members

--- Lab03/AimsProject/DigitalVideoDisc.java
+++ Lab03/AimsProject/DigitalVideoDisc.java
@@ -7,14 +7,23 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
 
+    private static int nbDigitalVideoDiscs = 0;
+    private int id = 0;
+
     public DigitalVideoDisc(String title) {
         this.title = title;
+        
+        nbDigitalVideoDiscs += 1;
+        this.id = nbDigitalVideoDiscs;
     }
 
     public DigitalVideoDisc(String category, String title, double cost) {
         this.category = category;
         this.title = title;
         this.cost = cost;
+
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
     public DigitalVideoDisc(String director, String category, String title, double cost) {
@@ -22,6 +31,9 @@ public class DigitalVideoDisc {
         this.category = category;
         this.title = title;
         this.cost = cost;
+
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
     public DigitalVideoDisc(String title, String category, String director, int length, double cost) {
@@ -30,6 +42,9 @@ public class DigitalVideoDisc {
         this.director = director;
         this.length = length;
         this.cost = cost;
+
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
     public String getTitle() {
